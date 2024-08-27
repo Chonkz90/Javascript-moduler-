9c39a43dab04854c04f9e82674fe80abd90ad43c
Chonkz90 <zahlerdal@yahoo.com>
2024-08-27 12:06:28 +0200

    lunsj

---
+++ basicTodoApplication.js
@@ -0,0 +1,13 @@
+// Todolist application med metode brukt opp til nå uten script modelering.
+
+
+// function fra TodoApplication.js
+function todoApplication() {
+
+    console.log("Her kommer en todo applikasjon.");
+}
+
+// Variabel og event listener fra script.js
+const addTodoButton = document.querySelector("#addTodoButton");
+
+addTodoButton.addEventListener("click",todoApplication);
